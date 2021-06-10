bb6230eb6f8be57552ea7e7fee1481b15dd9fe3d
mesi <mesi@gmail>
2021-06-10 21:49:37 +0300

    adding graphql(apollo client) to client

--- client/src/apollo-client/Mutation.js
+++ client/src/apollo-client/Mutation.js
@@ -2,7 +2,7 @@ import {gql} from '@apollo/client';
 
 export const addItem=gql`
     mutation add_product($title:String!,$Image:Upload!,$description:String!,$price:String!,$rating:Number!,$amount:Number!){
-        add_Book(name:$name,genre:$genre,authorId:$authorId){
+        add_product(name:$name,genre:$genre,authorId:$authorId){
             
         }
     }
